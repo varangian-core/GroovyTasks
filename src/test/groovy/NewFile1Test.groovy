67c4ddb13a4ac43b6f066d7f1037311db9920579
varangian-core <96369233+varangian-core@users.noreply.github.com>
2023-09-11 14:17:21 -0700

    My tests

---
+++ src/test/groovy/NewFile1Test.groovy
@@ -0,0 +1,12 @@
+
+    class NewFile1Test extends Specification {
+        def "test NewFile1"() {
+            given:
+            // setup statement here
+            when:
+            // action statements here
+            then:
+            // assertion statements here
+        }
+    }
+    
---
+++ src/test/groovy/NewFileTest.groovy
@@ -0,0 +1,12 @@
+
+    class NewFileTest extends Specification {
+        def "test NewFile"() {
+            given:
+            // setup statement here
+            when:
+            // action statements here
+            then:
+            // assertion statements here
+        }
+    }
+    
